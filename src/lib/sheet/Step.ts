3287ab0fc3e7d83dcb10178c8fe3bb4169c74ba0
KaianGuedes <euguedes1@gmail.com>
2025-08-16 20:39:32 -0300

    feat: getter & setter for step's selected option

--- src/lib/sheet/Step.ts
+++ src/lib/sheet/Step.ts
@@ -4,10 +4,25 @@ export class Step {
     uuid: string;
     label: string;
     options: Option[] = [];
+    selectedOptionIndex: number | undefined;
 
     constructor(label: string, options: Option[]) {
         this.label = label;
         this.options = options;
         this.uuid = crypto.randomUUID();
+        this.selectedOptionIndex = undefined
+    }
+
+    setSelectedOption(index: number) {
+        this.selectedOptionIndex = index;
+    }
+
+    getSelectedOption(): Option | undefined {
+        if (this.selectedOptionIndex === undefined) return undefined;
+        return this.options[this.selectedOptionIndex];
+    }
+
+    haveSelectedOption(): boolean {
+        return (this.selectedOptionIndex !== undefined);
     }
 }
